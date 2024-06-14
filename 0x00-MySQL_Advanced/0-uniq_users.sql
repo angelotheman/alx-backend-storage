c30b96e7461768f25352620a1710d7ec1e020b58
Kwabena Twumasi <ktwum721@gmail.com>
2024-06-14 14:50:21 +0000

    Uniq users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Create a table users
 
 CREATE TABLE IF NOT EXISTS users (
-	id INT,
-	email VARCHAR(255),
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
 );
