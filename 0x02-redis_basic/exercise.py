852dd08e977d3a25405c9ff9fa18d68df23fa21e
Kwabena Twumasi <ktwum721@gmail.com>
2024-06-20 01:58:44 +0000

    Updated for task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -32,3 +32,30 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+
+
+    def get(self, key: str, fn: Callable = None) -> Union[bytes, str, int, None]:
+        """
+        Convert data back to the desired format
+        """
+        data = self._redis.get(key)
+
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+
+    def get_str(self, key: str) -> str:
+        """
+        Parametrize the cache get
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+
+    def get_int(self, key: str) -> int:
+        """
+        Parametrize the cache get
+        """
+        return self.get(key, fn=int)
