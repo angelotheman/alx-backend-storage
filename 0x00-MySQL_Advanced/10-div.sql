3e687399ce98e1e7475b435f15555a0fa8f321f4
Kwabena Twumasi <ktwum721@gmail.com>
2024-06-25 18:36:46 +0000

    ✅ Divisor Done

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,19 @@
+-- Function for division
+
+DROP FUNCTION IF EXISTS SafeDiv;
+
+DELIMITER //
+
+CREATE FUNCTION SafeDiv (a INT, b, INT)
+RETURNS FLOAT
+BEGIN
+	DECLARE result FLOAT default 0;
+
+	IF b != 0 THEN
+		SET result = a / b;
+	END IF;
+
+	RETURN result;
+END //
+
+DELIMITER ;
