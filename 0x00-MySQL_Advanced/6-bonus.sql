3c4e9f10899d81345f24276e792768c00d793882
Kwabena Twumasi <ktwum721@gmail.com>
2024-06-25 16:48:15 +0000

    Bonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -4,7 +4,7 @@ DROP PROCEDURE IF EXISTS AddBonus;
 
 DELIMITER //
 
-CREATE PROCEDURE (
+CREATE PROCEDURE AddBonus (
 	IN user_id INT,
 	IN project_name VARCHAR(255),
 	IN score INT
@@ -19,9 +19,9 @@ BEGIN
 	IF project_id IS NULL THEN
 		INSERT INTO projects (name) VALUES (project_name);
 		SET project_id = LAST_INSERT_ID();
-	END IF
+	END IF;
 
-	INSERT INTO corrections (user_i, project_id, score) VALUES (user_id, project_id, score);
+	INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 END //
 
-DELIMITER;
+DELIMITER ;
