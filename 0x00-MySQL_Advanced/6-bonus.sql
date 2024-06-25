4720b1792b971d237e4d9fc9dc40e0ba7b1f87b4
Kwabena Twumasi <ktwum721@gmail.com>
2024-06-25 16:45:10 +0000

    Bonus

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,27 @@
+-- Add new correction for students stored procedure
+
+DROP PROCEDURE IF EXISTS AddBonus;
+
+DELIMITER //
+
+CREATE PROCEDURE (
+	IN user_id INT,
+	IN project_name VARCHAR(255),
+	IN score INT
+)
+BEGIN
+	DECLARE project_id INT;
+
+	SELECT id INTO project_id
+	FROM projects
+	WHERE name = project_name;
+
+	IF project_id IS NULL THEN
+		INSERT INTO projects (name) VALUES (project_name);
+		SET project_id = LAST_INSERT_ID();
+	END IF
+
+	INSERT INTO corrections (user_i, project_id, score) VALUES (user_id, project_id, score);
+END //
+
+DELIMITER;
