261924938e6a6e43dace06c17198f0900db6eccf
Kwabena Twumasi <ktwum721@gmail.com>
2024-06-25 17:26:51 +0000

    Average score settled

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,25 @@
+-- Compute average score for a student
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser (
+	IN user_id INT
+)
+BEGIN
+	DECLARE avg_score FLOAT;
+
+	-- Calculate average score from the corrections table
+	SELECT AVG(score) INTO avg_score
+	FROM  corrections
+	WHERE user_id = user_id;
+
+	-- Update the users table with the calculated average score
+	UPDATE users
+	SET average_score = avg_score
+	WHERE id = users_id;
+
+END //
+
+DELIMITER ;
