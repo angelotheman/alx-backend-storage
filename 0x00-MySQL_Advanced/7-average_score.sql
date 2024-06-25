a3796e0393ec969d51b9bc4104ff073368e923d3
Kwabena Twumasi <ktwum721@gmail.com>
2024-06-25 18:14:23 +0000

    Updated

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -13,12 +13,12 @@ BEGIN
 	-- Calculate average score from the corrections table
 	SELECT AVG(score) INTO avg_score
 	FROM  corrections
-	WHERE user_id = user_id;
+	WHERE corrections.user_id = user_id;
 
 	-- Update the users table with the calculated average score
 	UPDATE users
 	SET average_score = avg_score
-	WHERE id = user_id;
+	WHERE users.id = user_id;
 
 END //
 
