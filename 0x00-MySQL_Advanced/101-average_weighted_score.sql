3b37ecbdc29c7ea75a0c53482e35506463d3d2e5
Kwabena Twumasi <ktwum721@gmail.com>
2024-06-25 22:30:15 +0000

    Average weighted score

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,33 @@
+-- Compute the Average Weighted Score for all users
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+	DECLARE done INT DEFAULT 0;
+	DECLARE user_id INT;
+
+	-- Declare a cursor to iterate through all users
+	DECLARE cur CURSOR FOR SELECT id FROM users;
+	
+	-- Declare a NOT FOUND handler to exit the loop
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+
+	OPEN cur;
+
+	read_loop: LOOP
+		FETCH cur INTO user_id;
+
+		IF done THEN
+			LEAVE read_loop;
+		END IF;
+
+
+		CALL ComputeAverageWeightedScoreForUsers(user_id);
+	END LOOP;
+
+	CLOSE cur;
+END //
+
+DELIMITER ;
