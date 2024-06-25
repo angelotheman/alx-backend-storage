d7e4913506331af28dc377ae42c6c570bc065fe5
Kwabena Twumasi <ktwum721@gmail.com>
2024-06-25 18:26:28 +0000

    🔧 fixed

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,2 @@
--- Creates an index idx_name_first on the table names and the first letter of name.
-CREATE INDEX idx_name_first ON names(name(1), score);
+-- Creates an index on first letter of name and score
+CREATE INDEX idx_name_first_score ON names(name(1), score);
