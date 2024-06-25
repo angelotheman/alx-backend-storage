78454c7d24439b13caddbe47f3655a4ff4c843b1
Kwabena Twumasi <ktwum721@gmail.com>
2024-06-25 16:13:34 +0000

    Valid email updated

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,13 +1,16 @@
 -- Email Validation
+DROP TRIGGER IF EXISTS validate_email;
 
 DELIMITER //
 
 CREATE TRIGGER validate_email
-AFTER UPDATE ON users
+BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
 	IF OLD.email != NEW.email THEN
 		SET NEW.valid_email = 0;
+	ELSE
+		SET NEW.valid_email = NEW.valid_email;
 	END IF;
 END //
 
